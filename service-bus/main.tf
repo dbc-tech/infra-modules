4449a4cfa42ff33b5ccb7fdd00073b08f4d71686
Aniket Patwardhan <anikpat@gmail.com>
2022-10-31 15:51:35 +1100

    fix type

--- service-bus/main.tf
+++ service-bus/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_servicebus_namespace" "service-bus" {
-  name                = "sb-${var.environment}-${var.domain}-${var.instace_version}"
+  name                = "sb-${var.environment}-${var.domain}-${var.instance_version}"
   location            = var.location
   resource_group_name = var.resource_group_name
   sku                 = "Standard"
