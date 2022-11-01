a822faea226b3f9b197b01ba10bdb1e72e25eb87
Aniket Patwardhan <anikpat@gmail.com>
2022-11-02 09:15:35 +1100

    its an output

--- log-analytics/outputs.tf
+++ log-analytics/outputs.tf
@@ -1,7 +1,7 @@
-variable "log_analytics_workspace_id" {
+output "log_analytics_workspace_id" {
   value = azurerm_log_analytics_workspace.log-analytics.id
 }
 
-variable "log_analytics_shared_key" {
+output "log_analytics_shared_key" {
   value = azurerm_log_analytics_workspace.log-analytics.primary_shared_key
 }
