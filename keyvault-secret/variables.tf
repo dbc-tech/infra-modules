093b21155fa48a2e867f5749fedb9c7d5e25a2d8
Aniket Patwardhan <anikpat@gmail.com>
2022-11-04 13:30:39 +1100

    remove unrequired variables

--- keyvault-secret/variables.tf
+++ keyvault-secret/variables.tf
@@ -1,33 +1,3 @@
-variable "location" {
-  default = "australiaeast"
-  description = "The location/region where the resources in this module should be created."
-}
-
-variable "domain" {
-  type = string
-  description = "The domain will be the name for the project or shared for shared infrastructure."
-}
-
-variable "environment" {
-  type = string
-  description = "The environment name to use for the resources in this module."
-}
-
-variable "resource_group_id" {
-  type = string
-  description = "The resource group id to use for the resources in this module."
-}
-
-variable "resource_group_name" {
-  type = string
-  description = "The resource group name to use for the resources in this module."
-}
-
-variable "instance_version" {
-  default = "01"
-  description = "The instance version to use for the resources in this module."
-}
-
 variable "secret_name" {
   type = string
   description = "The secret name to save in keyvault."
