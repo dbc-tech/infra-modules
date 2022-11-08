d5c529102299ed614d1f7e5c639ada2275ea13cb
Aniket Patwardhan <anikpat@gmail.com>
2022-11-08 13:16:14 +1100

    add virtual network configuration

---
+++ virtual-network/main.tf
@@ -0,0 +1,10 @@
+resource "azurerm_virtual_network" "virtual-network" {
+  name                = var.name == null ? "ng-${var.environment}-${var.domain}-${var.instance_version}" : var.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  address_space       = var.address_space
+
+  tags = {
+    environment = "Production"
+  }
+}
