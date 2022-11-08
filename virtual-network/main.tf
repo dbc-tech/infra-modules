605195c6f9236a2c3c7f53e1cbc2afe13e69c57b
Aniket Patwardhan <anikpat@gmail.com>
2022-11-08 13:26:16 +1100

    correct the name

--- virtual-network/main.tf
+++ virtual-network/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_virtual_network" "virtual-network" {
-  name                = var.name == null ? "ng-${var.environment}-${var.domain}-${var.instance_version}" : var.name
+  name                = var.name == null ? "vnet-${var.environment}-${var.domain}-${var.instance_version}" : var.name
   location            = var.location
   resource_group_name = var.resource_group_name
   address_space       = var.address_space
