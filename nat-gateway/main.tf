f1b8e23437a279ae2bb7bc97f9b294020073a956
Aniket Patwardhan <anikpat@gmail.com>
2022-11-09 17:50:14 +1100

    add nat gateway modules

---
+++ nat-gateway/main.tf
@@ -0,0 +1,5 @@
+resource "azurerm_nat_gateway" "nat-gateway" {
+  name                = var.name == null ? "ng-${var.environment}-${var.domain}-${var.instance_version}" : var.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+}
---
+++ nat-gateway/variables.tf
@@ -0,0 +1,32 @@
+variable "name" {
+  type        = string
+  default     = null
+  description = "Name of the resource"
+}
+
+variable "resource_group_name" {
+  type        = string
+  description = "Name of the resource group where the resource will be created"
+}
+
+variable "location" {
+  type        = string
+  default     = "australiaeast"
+  description = "Location of the resource"
+}
+
+variable "domain" {
+  type        = string
+  description = "The domain will be the name for the project or shared for shared infrastructure."
+}
+
+variable "environment" {
+  type        = string
+  description = "The environment name to use for the resources in this module."
+}
+
+variable "instance_version" {
+  type        = string
+  default     = "01"
+  description = "The instance version to use for the resources in this module."
+}
