880142af24b674c89f46c723b7d1cfb99d39b614
Aniket Patwardhan <anikpat@gmail.com>
2022-11-09 18:02:27 +1100

    add tags to nat-gateway

--- nat-gateway/main.tf
+++ nat-gateway/main.tf
@@ -2,4 +2,9 @@ resource "azurerm_nat_gateway" "nat-gateway" {
   name                = var.name == null ? "ng-${var.environment}-${var.domain}-${var.instance_version}" : var.name
   location            = var.location
   resource_group_name = var.resource_group_name
+
+  tags = {
+    environment = var.environment
+    domain      = var.domain
+  }
 }
