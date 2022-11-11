0f2491cfd8acce7b559e4d3f9c0788359daf969e
Aniket Patwardhan <anikpat@gmail.com>
2022-11-11 13:13:01 +1100

    lets see if adding topics and subscriptions together works better

--- service-bus-topic/main.tf
+++ service-bus-topic/main.tf
@@ -2,3 +2,12 @@ resource "azurerm_servicebus_topic" "service-bus-topic" {
   name         = var.name
   namespace_id = var.service_bus_namespace_id
 }
+
+resource "azurerm_servicebus_subscription" "topic_subscription" {
+  count                                     = length(var.subscription_list)
+  name                                      = element(var.subscription_list, count.index)
+  topic_id                                  = azurerm_servicebus_topic.service-bus-topic.id
+  max_delivery_count                        = 1
+  dead_lettering_on_message_expiration      = true
+  dead_lettering_on_filter_evaluation_error = true
+}
--- service-bus-topic/variables.tf
+++ service-bus-topic/variables.tf
@@ -7,3 +7,9 @@ variable "service_bus_namespace_id" {
   type        = string
   description = "The ID of the ServiceBus Namespace to create this queue in. Changing this forces a new resource to be created."
 }
+
+variable "subscription_list" {
+  type = list(string)
+  default = null
+    description = "A list of subscriptions to create for a topic."
+}
