836ea52a049d057853f5fc2bae356280d3ea8c44
Aniket Patwardhan <anikpat@gmail.com>
2022-11-14 17:13:15 +1100

    make container registry private

--- container-registry/main.tf
+++ container-registry/main.tf
@@ -1,9 +1,10 @@
 resource "azurerm_container_registry" "container-registry" {
-  name                = "acr${var.environment}dbc${var.domain}${var.instance_version}"
-  resource_group_name = var.resource_group_name
-  location            = var.location
-  sku                 = var.sku
-  admin_enabled       = true
+  name                          = "acr${var.environment}dbc${var.domain}${var.instance_version}"
+  resource_group_name           = var.resource_group_name
+  location                      = var.location
+  sku                           = var.sku
+  admin_enabled                 = true
+  public_network_access_enabled = false
   tags = {
     environment = var.environment
     domain      = var.domain
