ab7676294a8bbba2e635677aba3666799a544043
Aniket Patwardhan <anikpat@gmail.com>
2022-11-15 10:39:09 +1100

    add admin username and password as output

---
+++ container-registry/outputs.tf
@@ -0,0 +1,7 @@
+output "admin-username" {
+  value = azurerm_container_registry.container-registry.admin_username
+}
+
+output "admin-password" {
+  value = azurerm_container_registry.container-registry.admin_password
+}
