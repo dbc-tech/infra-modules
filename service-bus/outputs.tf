b5b3ec79b362052f6c63056c91d157652618215d
Aniket Patwardhan <anikpat@gmail.com>
2022-11-16 09:33:55 +1100

    add connection_string as output for service bus

--- service-bus/outputs.tf
+++ service-bus/outputs.tf
@@ -1,3 +1,7 @@
 output "service_bus_namepace_id" {
   value = azurerm_servicebus_namespace.service-bus.id
 }
+
+output "default_primary_connection_string" {
+  value = azurerm_servicebus_namespace.service-bus.default_primary_connection_string
+}
