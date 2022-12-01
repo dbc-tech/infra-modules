42aad21f4a47db5c6c2fa0977e8e75778266a35e
Aniket Patwardhan <anikpat@gmail.com>
2022-12-01 14:22:34 +1100

    add connection string

--- application-insights/outputs.tf
+++ application-insights/outputs.tf
@@ -5,3 +5,7 @@ output "instrumentation_key" {
 output "app_id" {
   value = azurerm_application_insights.application-insights.app_id
 }
+
+output "connection_string" {
+  value = azurerm_application_insights.application-insights.connection_string
+}
