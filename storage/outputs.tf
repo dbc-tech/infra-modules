0919c019e852e1f1a23a24a7d25b8f34ad83e78d
Aniket Patwardhan <anikpat@gmail.com>
2022-12-01 14:26:12 +1100

    add primary connection string output

--- storage/outputs.tf
+++ storage/outputs.tf
@@ -5,3 +5,7 @@ output "storage_account_access_key" {
 output "storage_account_name" {
   value = azurerm_storage_account.storage.name
 }
+
+output "primary_connection_string" {
+  value = azurerm_storage_account.storage.primary_connection_string
+}
